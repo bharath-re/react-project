0825764e08444ab0f0823e8b1c278d88563bfddc
bharath-re <bharathb.w75@gmail.com>
2024-10-24 17:39:02 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GridExample from './components/dataApperance/GridExample'; // Correct path and name
+
+function App() {
+  return (
+    <div className="App">
+      <title>React App</title>
+      <GridExample></GridExample>
+    </div>
+  );
+}
+
+export default App;
